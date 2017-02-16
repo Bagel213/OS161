ff4a4d9548f08cf398cbbf8b54075a9ef76be772
Philip Taggart <philip.j.taggart@gmail.com>
2017-02-16 14:03:20 -0500

    began coding locks

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -76,6 +76,9 @@ struct lock {
         char *lk_name;
         HANGMAN_LOCKABLE(lk_hangman);   /* Deadlock detector hook. */
         // add what you need here
+        
+        int isHeld; // Boolean value to show whether lock is held or not
+
         // (don't forget to mark things volatile as needed)
 };
 
